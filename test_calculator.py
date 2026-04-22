1bd0058f82ff1be59496363d51822f93eee98895
Kanchan Chaudhary <Kanchan2.Chaudhary@live.uwe.ac.uk>
2026-04-22 07:30:56 +0545

    commit

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
 
     def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(5, 3), 1)
 
     def test_multiply(self):
         self.assertEqual(multiply(2, 3), 6)
